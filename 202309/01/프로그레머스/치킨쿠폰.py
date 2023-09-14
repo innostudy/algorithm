eb0b47ffe85bea7ef52ac855c680e294d1acbad0
chosanghyeon <whtkdgusdldi@naver.com>
2023-09-14 19:55:41 +0900

    test

--- 202309/01/프로그레머스/치킨쿠폰.py
+++ 202309/01/프로그레머스/치킨쿠폰.py
@@ -2,6 +2,8 @@
 # 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 
 # 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수
 # 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+
+#
 def solution(chicken):
     total = 0
     while chicken >= 10:
@@ -9,3 +11,4 @@ def solution(chicken):
         total = total + quotient;
         chicken = remainder + quotient;
     return total
+
