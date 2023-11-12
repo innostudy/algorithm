82a07edf2eddddd2e81f2b2fafd49609e2cfb9a9
acho-sang <whwhssk@gmail.com>
2023-11-12 20:28:26 +0900

    조상현 최소직사각형 feat

---
+++ acho-sang/20231112/최소직사각형.py
@@ -0,0 +1,16 @@
+# 가장 긴 가로 길이와 세로 길이가 각각 80, 70이기 때문에 80(가로) x 70(세로) 크기의 지갑을 만들면 모든 명함들을 수납할 수 있습니다. 하지만 2번 명함을 가로로 눕혀 수납한다면 80(가로) x 50(세로) 크기의 지갑으로 모든 명함들을 수납할 수 있습니다. 이때의 지갑 크기는 4000(=80 x 50)입니다.
+
+# 모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다. 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
+def solution(sizes):
+    long_size = []
+    short_size = []
+    for i in sizes:
+        if(i[0] > i[1]):
+            long_size.append(i[0])
+            short_size.append(i[1])
+        else:
+            long_size.append(i[1])
+            short_size.append(i[0])
+
+    answer = max(long_size) * max(short_size)
+    return answer
