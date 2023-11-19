82d81a89ad0b16cf4d2004ad284e278ca4bbfe0e
acho-sang <whwhssk@gmail.com>
2023-11-19 21:26:36 +0900

    조상현 소수찾기 add sol

--- acho-sang/20231119/소수찾기.py
+++ acho-sang/20231119/소수찾기.py
@@ -16,3 +16,12 @@ def solution(n):
         if(is_prime):
             answer += 1
     return answer
+
+
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
