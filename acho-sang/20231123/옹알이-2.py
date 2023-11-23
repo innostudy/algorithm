4ba1f8846bdec6b69e1e9d3724431f0cd7760da0
acho-sang <whwhssk@gmail.com>
2023-11-23 15:08:42 +0900

    조상현 옹알이-2 other_sol

--- acho-sang/20231123/옹알이-2.py
+++ acho-sang/20231123/옹알이-2.py
@@ -17,3 +17,13 @@ def solution(babbling):
             answer += 1
 
     return answer
+
+def solution(babbling):
+    answer = 0
+    for i in babbling:
+        for j in ['aya','ye','woo','ma']:
+            if j*2 not in i:
+                i=i.replace(j,' ')
+        if len(i.strip())==0:
+            answer +=1
+    return answer
