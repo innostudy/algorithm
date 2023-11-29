471fc8952eafebba6db3739180d4de4d8b96e2b2
acho-sang <whwhssk@gmail.com>
2023-11-29 15:51:46 +0900

    조상현 체육복 1h

---
+++ acho-sang/20231129/체육복.py
@@ -0,0 +1,24 @@
+# 점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+
+# 전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+def solution(n, lost, reserve):
+    answer = 0
+    lost.sort()
+    state_list = [1]*n
+
+    for i in reserve:
+        state_list[i-1] += 1
+    for i in lost:
+        if(state_list[i-1] == 2):
+            state_list[i-1] = 1
+        elif(i >= 2 and state_list[i-2] == 2):
+            state_list[i-1] = 1
+            state_list[i-2] = 1
+        elif(i <= n-1 and state_list[i] == 2 and i+1 not in lost):
+            state_list[i] = 1
+            state_list[i-1] = 1
+        else:
+            state_list[i-1] = 0
+    
+            
+    return n-state_list.count(0)
